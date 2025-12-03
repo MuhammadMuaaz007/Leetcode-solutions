95c0ea4c07cb1fda26d71aa68ecdb5a0d14d59c7
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-03 11:11:15 +0500

    Add solution for longest consecutive sequence problem
    Implement longestConsecutive function to find the length of the longest consecutive sequence in an array.

--- LeetCode/Longest-Consecutive-Sequence/Solution.cpp
+++ LeetCode/Longest-Consecutive-Sequence/Solution.cpp
@@ -1,1 +1,23 @@
-[1, 2, 3, 4]
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end()); // insert all numbers at once
+        int maxLen = 0;
+
+        for(int n : numSet) {  // iterate only unique numbers
+            if(numSet.find(n - 1) == numSet.end()) { // start of sequence
+                int current = n;
+                int length = 1;
+
+                while(numSet.find(current + 1) != numSet.end()) {
+                    current++;
+                    length++;
+                }
+
+                maxLen = max(maxLen, length);
+            }
+        }
+
+        return maxLen;
+    }
+};
