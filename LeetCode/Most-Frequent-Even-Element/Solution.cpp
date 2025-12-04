d2a81e4aefd46590fae6a05c178bc35a4cb0303d
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-04 11:03:04 +0500

    Refactor mostFrequentEven function implementation
    Refactor the mostFrequentEven function to use unordered_map for frequency counting and improve logic for determining the most frequent even number.

--- LeetCode/Most-Frequent-Even-Element/Solution.cpp
+++ LeetCode/Most-Frequent-Even-Element/Solution.cpp
@@ -1,13 +1,32 @@
-map<int, int> mp;
-
-        for (const auto it : nums) {
-                mp[it]++;
-        }
-        int ans = -1, max = - 1;
-
-        for (const auto m : mp) {
-            if (m.first % 2 == 0 && m.second > max) {
-                ans = m.first;
-                max = m.second;
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int num : nums) {
+            if(num % 2 == 0) {
+                freq[num]++;
             }
         }
+        
+        if(freq.empty()) return -1; // No even number found
+        
+        int ans = -1;
+        int highestFreq = 0;
+ 
+        for(auto &it : freq) {
+            int number = it.first;
+            int count = it.second;
+            
+            // Condition 1: bigger frequency
+            // Condition 2: if same frequency, choose smaller number
+            if(count > highestFreq || 
+               (count == highestFreq && number < ans))
+            {
+                highestFreq = count;
+                ans = number;
+            }
+        }
+        
+        return ans;
+    }
+};
