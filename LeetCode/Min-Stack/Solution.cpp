bb2ab786223f0fba7783fd029f825a75c091193a
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-10 11:49:57 +0500

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.cpp
+++ LeetCode/Min-Stack/Solution.cpp
@@ -2,29 +2,33 @@
 2    stack<pair<int,int>> st;
 3
 4public:
-5    void push(int n) {
-6        int minVal = st.empty() ? n : min(n, st.top().second);
-7        st.push({n, minVal});
-8    }
-9
-10    void pop() {
-11        st.pop();
+5    void push(int val) {
+6       if(st.empty()){
+7        st.push({val,val});
+8       }else{
+9        int minVal=min(val,st.top().second);
+10        st.push({val,minVal});
+11       }
 12    }
 13
-14    int top() {
-15        return st.top().first;
+14    void pop() {
+15        st.pop();
 16    }
 17
-18    int getMin() {
-19        return st.top().second;
+18    int top() {
+19        return st.top().first;
 20    }
-21};
-22
-23/**
-24 * Your MinStack object will be instantiated and called as such:
-25 * MinStack* obj = new MinStack();
-26 * obj->push(val);
-27 * obj->pop();
-28 * int param_3 = obj->top();
-29 * int param_4 = obj->getMin();
-30 */
+21
+22    int getMin() {
+23        return st.top().second;
+24    }
+25};
+26
+27/**
+28 * Your MinStack object will be instantiated and called as such:
+29 * MinStack* obj = new MinStack();
+30 * obj->push(val);
+31 * obj->pop();
+32 * int param_3 = obj->top();
+33 * int param_4 = obj->getMin();
+34 */
