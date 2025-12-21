e47f2e28d67e8ca0a51ced5bc8da4c2097eb72ae
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-21 17:39:58 +0500

    Add path simplification logic in Solution.cpp
    Implement a function to simplify Unix-style paths using a stack.

--- LeetCode/Simplify-Path/Solution.cpp
+++ LeetCode/Simplify-Path/Solution.cpp
@@ -1,1 +1,33 @@
-                           APPROACH/PSUEDO CODE
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string word = "";
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (word == "" || word == ".") {
+                    // do nothing
+                }
+                else if (word == "..") {
+                    if (!st.empty())
+                        st.pop();
+                }
+                else {
+                    st.push(word);
+                }
+                word = ""; // reset for next folder
+            }
+            else {
+                word += path[i];
+            }
+        }
+        // build result
+        string result = "";
+        while (!st.empty()) {
+            result = "/" + st.top() + result;
+            st.pop();
+        }
+
+        return result == "" ? "/" : result;
+    }
+};
