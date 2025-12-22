c40d77a662519452e49e72024ef1daa8404b76ec
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-22 21:51:34 +0500

    Add FreqStack class for maximum frequency stack
    Implemented a frequency stack that supports push and pop operations based on the frequency of elements.

--- LeetCode/Maximum-Frequency-Stack/Solution.cpp
+++ LeetCode/Maximum-Frequency-Stack/Solution.cpp
@@ -1,2 +1,28 @@
-["FreqStack","push","push","push","push","pop", "pop", "push", "push", "push", "pop", "pop", "pop"]
-[[],[1], [1], [1], [2], [], [], [2], [2], [1], [], [], []]
+ class FreqStack {
+ private:
+    unordered_map<int, int> freq;                 // element -> frequency
+    unordered_map<int, stack<int>> group;         // frequency -> stack of elements
+    int maxFreq;                                  // current maximum frequency
+
+ public:
+    FreqStack() {
+        maxFreq = 0;
+    }
+    void push(int val) {
+        int f = ++freq[val];         
+        group[f].push(val);           
+        maxFreq = max(maxFreq, f);    
+    }
+
+    int pop() {
+        int val = group[maxFreq].top();
+        group[maxFreq].pop();        
+        freq[val]--;                 
+
+        if (group[maxFreq].empty()) { 
+            maxFreq--;
+        }
+
+        return val;
+    }
+};
