ee79e22be71b289efa76553ef4bb9f6e685bfbf4
Muhammad Muaaz <mrmuaaz31@gmail.com>
2025-12-27 22:17:33 +0500

    Add solution for Reverse Linked List II

--- LeetCode/Reverse-Linked-List-II/Solution.cpp
+++ LeetCode/Reverse-Linked-List-II/Solution.cpp
@@ -17,19 +17,41 @@
 17        ListNode* preNode = NULL;
 18        ListNode* nextNode = NULL;
 19
-20        // 🔥 FIXED LOOP
-21        for (int i = 0; i <= right - left; i++) {
-22            nextNode = currNode->next;
-23            currNode->next = preNode;
-24            preNode = currNode;
-25            currNode = nextNode;
-26        }
-27
-28        // Reconnect
-29        leftPre->next = preNode;
-30        subListedHead->next = currNode;
-31
-32        return dummy->next;
-33    }
-34};
+20        for (int i = 0; i <= right - left; i++) {
+21            nextNode = currNode->next;
+22            currNode->next = preNode;
+23            preNode = currNode;
+24            currNode = nextNode;
+25        }
+26
+27        // Reconnect
+28        leftPre->next = preNode;
+29        subListedHead->next = currNode;
+30        
+31        return dummy->next;
+32    }
+33};
+34
 35
+36
+37/*We split the list into 3 parts:
+38
+39[Part A] → [Part B] → [Part C]
+40
+41
+42Part A: nodes before left
+43
+44Part B: nodes from left to right (this part will be reversed)
+45
+46Part C: nodes after right
+47
+48Then:
+49
+50Reverse Part B
+51
+52Connect:
+53
+54end of Part A → new head of Part B
+55
+56end of Part B → Part C*/
+57
