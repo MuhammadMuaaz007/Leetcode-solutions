ffe68929ef99d279fd6099f258702f6f33b08f61
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-01-18 22:35:09 +0500

    Implement FizzBuzz solution in C++

--- LeetCode/Fizz-Buzz/Solution.cpp
+++ LeetCode/Fizz-Buzz/Solution.cpp
@@ -1,1 +1,19 @@
-    return [[str(i), 'Fizz', 'Buzz', 'FizzBuzz'][(i%3==0)+(i%5==0)*2] for i in range(1, n+1)]
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.push_back("Buzz");
+            } else {
+                result.push_back(to_string(i));
+            }
+        }
+
+        return result;
+    }
