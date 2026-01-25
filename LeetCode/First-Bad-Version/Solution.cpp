ca7524f904fd9266bb77b6ab3762bc4a17e17b47
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-01-25 19:20:40 +0500

    Add firstBadVersion function using binary search
    Implement binary search to find the first bad version.

--- LeetCode/First-Bad-Version/Solution.cpp
+++ LeetCode/First-Bad-Version/Solution.cpp
@@ -1,1 +1,21 @@
-Input: n = 5, bad = 4
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2; //    int mid = (high + low) / 2; int low = 2,000,000,000;
+// int high = 2,000,000,001;
+// (low + high) = 4,000,000,001 → this overflows 32-bit int!
+            if (isBadVersion(mid)) {
+                high = mid; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return low;
+    }
+};
