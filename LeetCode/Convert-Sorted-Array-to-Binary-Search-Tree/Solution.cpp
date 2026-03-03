5d955eb04a819f33677ecd459a591587ec2e1183
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-03-03 12:11:29 +0500

    Add overflow comment in sortedArrayToBST method
    Refactor the build function to include a comment about avoiding overflow when calculating the mid index.

--- LeetCode/Convert-Sorted-Array-to-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Convert-Sorted-Array-to-Binary-Search-Tree/Solution.cpp
@@ -9,23 +9,23 @@
 9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 10 * };
 11 */
-12class Solution {
-13public:
-14    TreeNode* sortedArrayToBST(vector<int>& nums) {
-15        return build(nums, 0, nums.size() - 1);
-16    }
-17
-18private:
-19    TreeNode* build(vector<int>& nums, int l, int r) {
-20        if (l > r) return nullptr;
-21
-22        int mid = l + (r - l) / 2;  
-23        TreeNode* root = new TreeNode(nums[mid]);
-24
-25        // Recursively build left and right subtrees
-26        root->left = build(nums, l, mid - 1);
-27        root->right = build(nums, mid + 1, r);
-28
-29        return root;
-30    }
-31};
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode* build(vector<int>& nums, int l, int r) {
+        if (l > r) return nullptr;
+
+        int mid = l + (r - l) / 2;  // Avoid overflow
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        // Recursively build left and right subtrees
+        root->left = build(nums, l, mid - 1);
+        root->right = build(nums, mid + 1, r);
+
+        return root;
+    }
+};
