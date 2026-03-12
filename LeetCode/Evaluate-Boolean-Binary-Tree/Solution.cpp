bfaf5390fa43d3b5ec97a431361e03e2addef32d
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-03-12 23:21:06 +0500

    Add evaluateTree function for boolean tree evaluation
    Implement evaluateTree function to evaluate a boolean binary tree.

--- LeetCode/Evaluate-Boolean-Binary-Tree/Solution.cpp
+++ LeetCode/Evaluate-Boolean-Binary-Tree/Solution.cpp
@@ -1,7 +1,20 @@
-[3,3,2,0,1,0,0]
-[3,3,2,2,3,3,3,0,1,0,0,1,0,0,0]
-[2,3,3,1,1,0,1]
-[2,0,2,null,null,0,1]
-[2,1,3,null,null,1,0]
-[2,3,1,1,2,null,null,null,null,0,1]
-[2,3,1,1,2,null,null,null,null,0,1]
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        
+        // Leaf node
+        if(root->left == NULL && root->right == NULL) {
+            return root->val;
+        }
+
+        bool left = evaluateTree(root->left);
+        bool right = evaluateTree(root->right);
+
+        if(root->val == 2) {
+            return left || right;   // OR
+        }
+        else {
+            return left && right;   // AND
+        }
+    }
+};
