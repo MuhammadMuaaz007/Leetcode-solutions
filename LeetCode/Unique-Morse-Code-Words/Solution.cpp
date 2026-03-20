ab16b155bb694f63047d823df2bcefccb036936d
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-03-20 14:15:48 +0500

    Implement unique Morse code transformations
    Refactor the Morse code representation logic into a class method.

--- LeetCode/Unique-Morse-Code-Words/Solution.cpp
+++ LeetCode/Unique-Morse-Code-Words/Solution.cpp
@@ -1,1 +1,25 @@
-morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        
+        vector<string> morse = {
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+            "...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+
+        unordered_set<string> st;
+
+        for(string word : words) {
+            string code = "";
+
+            for(char c : word) {
+                code += morse[c - 'a']; // map letter to index
+            }
+
+            st.insert(code);
+        }
+
+        return st.size();
+    }
+};
