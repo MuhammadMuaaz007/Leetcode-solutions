34d60f1736e5e406c5e874791cf32998af234b58
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-03-30 22:06:25 +0500

    Refactor checkTree method to simplify logic

--- LeetCode/Root-Equals-Sum-of-Children/Solution.cpp
+++ LeetCode/Root-Equals-Sum-of-Children/Solution.cpp
@@ -1,16 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public boolean checkTree(TreeNode root) {
-if(root==null) return true;
-if(root.left==null&&root.right==null)
-     return true;
-    int sum=0;
-     if(root.left!=null)
-      sum+=root.left.val;
-        
-      if(root.right!=null) 
-      sum+=root.right.val;
-       
-        return
-(sum==root.val&&checkTree(root.left)&&checkTree(root.right));
+public:
+    bool checkTree(TreeNode* root) {
+        return root->val == root->left->val + root->right->val;
     }
-}
+};
