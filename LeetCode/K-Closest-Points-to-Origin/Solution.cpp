040123cf9f9a502d487a86ee2a6f9d079a02e238
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-04-14 12:36:25 +0500

    Update Solution.cpp

--- LeetCode/K-Closest-Points-to-Origin/Solution.cpp
+++ LeetCode/K-Closest-Points-to-Origin/Solution.cpp
@@ -1,3 +1,30 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(points, key=lambda x: x[0] * x[0] + x[1] * x[1])[:k]
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        // Max Heap
+        priority_queue<pair<int, vector<int>>> pq;
+
+        // Index-based loop
+        for (int i = 0; i < points.size(); i++) {
+            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            
+            pq.push({dist, points[i]});
+
+            // Keep only k closest points
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        // Store result
+        vector<vector<int>> ans;
+
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
