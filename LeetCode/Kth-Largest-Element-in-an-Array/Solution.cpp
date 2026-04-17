352d564f2b9827ae93d2aa5ed9258ce58765f677
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-04-17 21:52:29 +0500

    Format Solution.cpp for Kth Largest Element

--- LeetCode/Kth-Largest-Element-in-an-Array/Solution.cpp
+++ LeetCode/Kth-Largest-Element-in-an-Array/Solution.cpp
@@ -1,16 +1,16 @@
-1
-2class Solution {
-3public:
-4 int findKthLargest(vector<int>& nums, int k) {
-5    priority_queue<int, vector<int>, greater<int>> minHeap; // Min-heap
-6    
-7    for (int num : nums) {
-8        minHeap.push(num);
-9        if (minHeap.size() > k) {
-10            minHeap.pop();
-11        }
-12    }
-13    
-14    return minHeap.top();
-15}
-16};
+
+class Solution {
+public:
+ int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Min-heap
+    
+    for (int num : nums) {
+        minHeap.push(num);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+    
+    return minHeap.top();
+}
+};
