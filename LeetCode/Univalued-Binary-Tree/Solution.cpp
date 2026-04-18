f18cc69c6043c693e1cc0abeaf0a1e9373491457
Muhammad Muaaz <mrmuaaz31@gmail.com>
2026-04-18 22:57:10 +0500

    Refactor Solution class for univalued binary tree

--- LeetCode/Univalued-Binary-Tree/Solution.cpp
+++ LeetCode/Univalued-Binary-Tree/Solution.cpp
@@ -9,12 +9,12 @@
 9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 10 * };
 11 */
-12class Solution {
-13public:
-14    bool isUnivalTree(TreeNode* root) {
-15        if (!root) return true;
-16        if (root->left && root->left->val != root->val) return false;
-17        if (root->right && root->right->val != root->val) return false;
-18        return isUnivalTree(root->left) && isUnivalTree(root->right);
-19    }
-20};
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if (!root) return true;
+        if (root->left && root->left->val != root->val) return false;
+        if (root->right && root->right->val != root->val) return false;
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
+    }
+};
